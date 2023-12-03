fba83d42e9978a579925cb56a75f66bd5d930c98
CherniakYura <juracherniak@gmail.com>
2023-12-03 18:53:59 +0100

    Add generation

--- index.js
+++ index.js
@@ -180,3 +180,122 @@ function analyse(ast) {
 
   return result;    
 }
+
+function generate(ast, analysis) {
+  const code = {
+    variables: [],
+    create: [],
+    update: [],
+    destroy: [],
+  };
+
+  let counter = 1;
+  function traverse(node, parent) {
+    switch(node.type) {
+      case 'Element':{
+        const variableName = `${node.name}_${counter++}`;
+        code.variables.push(variableName);
+        code.create.push(
+          `${variableName} = document.createElement('${node.name}');`
+        )
+        node.attributes.forEach(attribute => {
+          traverse(attribute, variableName);
+        });
+        node.children.forEach(child => {
+          traverse(child, variableName);
+        });
+        code.create.push(`${parent}.appendChild(${variableName})`);
+        code.destroy.push(`${parent}.removeChild(${variableName})`);
+        break;
+      }
+      case 'Text': {
+        const variableName = `txt_${counter++}`;
+        code.variables.push(variableName);
+        code.create.push(
+          `${variableName} = document.createTextNode('${node.value}')`
+        );
+        code.create.push(`${parent}.appendChild(${variableName})`);
+        break;
+      }
+      case 'Attribute': {
+        if (node.name.startsWith('on:')) {
+          const eventName = node.name.slice(3);
+          const eventHandler = node.value.name;
+          code.create.push(
+            `${parent}.addEventListener('${eventName}', ${eventHandler});`
+          );
+          code.destroy.push(
+            `${parent}.removeEventListener('${eventName}', ${eventHandler});`
+          );
+        }
+        break;
+      }
+      case 'Expression':{
+        const variableName = `txt_${counter++}`;
+        const expression = node.expression.name;
+        code.variables.push(variableName);
+        code.create.push(
+          `${variableName} = document.createTextNode(${expression})`
+        );
+        code.create.push(`${parent}.appendChild(${variableName});`);
+        if (analysis.willChange.has(node.expression.name)) {
+          code.update.push(`if (changed.includes('${expression}')) {
+            ${variableName}.data = ${expression};
+          }`);
+        }
+        break;
+      }
+    }
+  }
+
+  ast.html.forEach(fragment => traverse(fragment, 'target'));
+
+  const { rootScope, map } = analysis;
+  let currentScope = rootScope;
+  estreewalker.walk(ast.script, {
+    enter(node) {
+      if (map.has(node)) currentScope = map.get(node);
+      if (
+        node.type === 'UpdateExpression' &&
+        currentScope.find_owner(node.argument.name) === rootScope &&
+        analysis.willUseInTemplate.has(node.argument.name)
+      ) {
+        this.replace({
+          type: 'SequenceExpression',
+          expressions: [
+            node,
+            acorn.parseExpressionAt(
+              `lifecycle.update(['${node.argument.name}'])`,
+              0,
+              {
+                ecmaVersion: 2022,
+              }
+            )
+          ]
+        })
+        this.skip();
+      }
+    },
+    leave(node) {
+      if (map.has(node)) currentScope = currentScope.parent;
+    }
+  });
+  return `
+    export default function() {
+      ${code.variables.map(v => `let ${v};`).join('\n')}
+      ${escodegen.generate(ast.script)}
+      const lifecycle = {
+        create(target) {
+          ${code.create.join('\n')}
+        },
+        update(changed) {
+          ${code.update.join('\n')}
+        },
+        destroy() {
+          ${code.destroy.join('\n')}
+        },
+      };
+      return lifecycle;
+    }
+  `
+}
