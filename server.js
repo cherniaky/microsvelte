cbb1ff4509e311079b4dac35c0ad59b054520654
CherniakYura <juracherniak@gmail.com>
2023-12-17 20:46:33 +0100

    Implement hydration

--- server.js
+++ server.js
@@ -1,8 +1,34 @@
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
 import appComponent from './ssr.js';
 import { createServer } from 'node:http';
 
 const server = createServer((req, res) => {
-    res.write(appComponent());
+    if (req.url === '/app.js') {
+        const appJsContent = fs.readFileSync(
+            path.join(fileURLToPath(import.meta.url), '../app.js'),
+            'utf-8'
+        );
+        res.setHeader('Content-Type', 'text/javascript');
+        res.write(appJsContent);
+        res.end();
+        return;
+    }
+
+    res.write(
+        `<html>
+    <body>
+      <div id="app">${appComponent()}</div>
+      <script type="module">
+        import App from './app.js';
+        //window.hydrate = () => {
+          App().create(document.querySelector('#app'));
+        //};
+      </script>
+    </body>
+  </html>`
+    );
     res.end();
 });
 server.listen(8000);
--- ssr.js
+++ ssr.js
@@ -1,14 +1,14 @@
 
-export default function() {
-    let double;
-    let quadruple;
-    let counter = 5;
-    const increment = () => {
-        counter += 1;
-    };
-    const decrement = () => counter--;
-    double = counter * 2;
-    quadruple = double * 2;
-    return `<button>Decrement</button><div>${counter} * 2 = ${double}</div><div>${double} * 2 = ${quadruple}</div><button>Increment</button>`;
-}
-
+    export default function() {
+      let double;
+let quadruple;
+      let counter = 5;
+const increment = () => {
+    counter += 1;
+};
+const decrement = () => counter--;
+      double = counter * 2;
+quadruple = double * 2;
+      return `<button>Decrement<!----></button><div>${ counter }<!----> * 2 = <!---->${ double }<!----></div><div>${ double }<!----> * 2 = <!---->${ quadruple }<!----></div><button>Increment<!----></button>`;
+    }
+  
